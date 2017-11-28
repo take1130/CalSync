3d57baba89ccf70c814b8485b9f8e3f6640fe3a9
take1130 <take1130@gmail.com>
2017-11-28 17:41:18 +0900

    インスタンスの作成処理をテスト実行前に変更

--- src/test/garoonScheduleTest.ts
+++ src/test/garoonScheduleTest.ts
@@ -5,25 +5,20 @@ import * as GaroonSchedule from "../app/garoonSchedule";
 
 describe("garoonSchedule", () => {
     let options: any;
+    let gs: GaroonSchedule.GaroonSchedule;
 
     before(() => {
         options = JSON.parse(fs.readFileSync("server.json", "utf-8"));
-    });
-
-    it("constructor test", () => {
-        const gs = new GaroonSchedule.GaroonSchedule({ url: options.garoon.url });
+        gs = new GaroonSchedule.GaroonSchedule({ url: options.garoon.url });
+        gs.authenticate(options.garoon.user, options.garoon.password);
     });
 
     it("getEventVersion test, no parameter", async () => {
-        const gs = new GaroonSchedule.GaroonSchedule({ url: options.garoon.url });
-        gs.authenticate(options.garoon.user, options.garoon.password);
         const versions = await gs.getEventVersion();
         console.log(versions);
     });
 
     it("getEventVersion test, has parameter", async () => {
-        const gs = new GaroonSchedule.GaroonSchedule({ url: options.garoon.url });
-        gs.authenticate(options.garoon.user, options.garoon.password);
         const versions = await gs.getEventVersion([{ attributes: { id: "732635", version: "1510634372" } }]);
         console.log(versions);
     });
