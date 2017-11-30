33a2fa111191cb7f9fb68802188bc40e3ea6eae4
take1130 <take1130@gmail.com>
2017-11-30 16:40:23 +0900

    CalDavクラスにカレンダー情報を取得する関数を追加

---
+++ src/app/caldav.ts
@@ -0,0 +1,112 @@
+import * as request from "request";
+import { soap } from "strong-soap";
+import { URL } from "url";
+import * as util from "util";
+
+export class CalDav {
+
+    /**
+     * getCurrentUserPrincipal
+     */
+    public static getCurrentUserPrincipal(url: URL, user: string, password: string) {
+        const xml: string = `
+        <d:propfind xmlns:d="DAV:">
+          <d:prop>
+            <d:current-user-principal />
+          </d:prop>
+        </d:propfind>
+        `;
+        const options: request.CoreOptions = {
+            method: "PROPFIND",
+            headers: {
+                Depth: "0",
+                Prefer: "return-minimal",
+            },
+            proxy: "",
+            body: xml,
+        };
+        return new Promise((resolve, reject) => {
+            request(url.toString(), options, (error: any, response: request.RequestResponse, body: any) => {
+                if (error) {
+                    reject(error);
+                }
+
+                console.log(body.toString());
+
+                const handler = new soap.XMLHandler();
+                let json = handler.xmlToJson(null, body.toString(), null);
+
+                resolve(json);
+            }).auth(user, password);
+        });
+    }
+
+    /**
+     * getCalendarHomeSet
+     */
+    public static getCalendarHomeSet(url: URL, user: string, password: string) {
+        const xml = `
+        <d:propfind xmlns:d="DAV:" xmlns:c="urn:ietf:params:xml:ns:caldav">
+          <d:prop>
+            <c:calendar-home-set />
+          </d:prop>
+        </d:propfind>
+        `;
+        const options: request.CoreOptions = {
+            method: "PROPFIND",
+            headers: {
+                Depth: "0",
+                Prefer: "return-minimal",
+            },
+            proxy: "",
+            body: xml,
+        };
+        return new Promise((resolve, reject) => {
+            request(url.toString(), options, (error: any, response: request.RequestResponse, body: any) => {
+                if (error) {
+                    reject(error);
+                }
+
+                const handler = new soap.XMLHandler();
+                const json = handler.xmlToJson(null, body.toString(), null);
+                resolve(json);
+            }).auth(user, password);
+        });
+    }
+
+    /**
+     * getCalendarComponentSet
+     */
+    public static getCalendarComponentSet(url: URL, user: string, password: string) {
+        const xml = `
+        <d:propfind xmlns:d="DAV:" xmlns:cs="http://calendarserver.org/ns/" xmlns:c="urn:ietf:params:xml:ns:caldav">
+          <d:prop>
+           <d:resourcetype />
+           <d:displayname />
+           <cs:getctag />
+           <c:supported-calendar-component-set />
+          </d:prop>
+        </d:propfind>
+        `;
+        const options: request.CoreOptions = {
+            method: "PROPFIND",
+            headers: {
+                Depth: "1",
+                Prefer: "return-minimal",
+            },
+            proxy: "",
+            body: xml,
+        };
+        return new Promise((resolve, reject) => {
+            request(url.toString(), options, (error: any, response: request.RequestResponse, body: any) => {
+                if (error) {
+                    reject(error);
+                }
+
+                const handler = new soap.XMLHandler();
+                const json = handler.xmlToJson(null, body.toString(), null);
+                resolve(json);
+            }).auth(user, password);
+        });
+    }
+}
