17553378594b9ec8883f361e8d4eb54dec2b7cbe
take1130 <take1130@gmail.com>
2017-12-04 17:34:29 +0900

    CalDavサーバーへの登録、削除、検索処理を追加

--- src/app/caldav.ts
+++ src/app/caldav.ts
@@ -4,7 +4,6 @@ import { URL } from "url";
 import * as util from "util";
 
 export class CalDav {
-
     /**
      * getCurrentUserPrincipal
      */
@@ -104,6 +103,115 @@ export class CalDav {
                 const json = handler.xmlToJson(null, body.toString(), null);
                 resolve(json);
             }).auth(user, password);
+        });
+    }
+
+    private url: URL;
+    private user: string;
+    private password: string;
+
+    constructor(url: URL, user: string, password: string) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    /**
+     * search
+     */
+    public search(field: string, id: string) {
+        const xml = `
+        <c:calendar-query xmlns:d="DAV:" xmlns:c="urn:ietf:params:xml:ns:caldav">
+          <d:prop>
+            <d:getetag />
+            <c:calendar-data />
+          </d:prop>
+          <c:filter>
+            <c:comp-filter name="VCALENDAR">
+              <c:comp-filter name="VEVENT">
+                <c:prop-filter name="%s">
+                  <c:text-match>%s</c:text-match>
+                </c:prop-filter>
+              </c:comp-filter>
+            </c:comp-filter>
+          </c:filter>
+        </c:calendar-query>
+        `;
+        const options: request.CoreOptions = {
+            method: "REPORT",
+            headers: {
+                Depth: "1",
+                Prefer: "return-minimal",
+            },
+            proxy: "",
+            body: util.format(xml, field, id),
+        };
+        return new Promise((resolve, reject) => {
+            request(this.url.toString(), options, (error: any, response: request.RequestResponse, body: any) => {
+                if (error) {
+                    reject(error);
+                }
+
+                const handler = new soap.XMLHandler();
+                const json = handler.xmlToJson(null, body.toString(), null);
+                resolve(json);
+            }).auth(this.user, this.password);
+        });
+    }
+
+    /**
+     * put
+     */
+    public put(ics: string, event: string, etag?: string) {
+        const options: request.CoreOptions = {
+            method: "PUT",
+            proxy: "",
+            body: event,
+        };
+
+        if (etag) {
+            options.headers = { "If-Match": etag };
+        }
+
+        const putUrl = new URL(ics, this.url);
+
+        return new Promise((resolve, reject) => {
+            request(putUrl.toString(), options, (error: any, response: request.RequestResponse, body: any) => {
+                if (error) {
+                    reject(error);
+                }
+
+                const handler = new soap.XMLHandler();
+                const json = handler.xmlToJson(null, body.toString(), null);
+                resolve(json);
+            }).auth(this.user, this.password);
+        });
+    }
+
+    /**
+     * delete
+     */
+    public delete(ics: string, etag: string) {
+        const options: request.CoreOptions = {
+            method: "DELETE",
+            proxy: "",
+            headers: {
+                "If-Match": etag,
+            },
+        };
+
+        const deleteUrl = new URL(ics, this.url);
+
+        return new Promise((resolve, reject) => {
+            request(deleteUrl.toString(), options, (error: any, response: request.RequestResponse, body: any) => {
+                if (error) {
+                    reject(error);
+                }
+
+                const handler = new soap.XMLHandler();
+                const json = handler.xmlToJson(null, body.toString(), null);
+                resolve(json);
+            }).auth(this.user, this.password);
         });
     }
 }
