b862d40b00cb795ca86562aeb7960c827d92a054
take1130 <take1130@gmail.com>
2017-12-14 16:06:49 +0900

    GaroonのスケジュールIDの保存と更新情報取得時に使用する処理を追加

--- src/app/main.ts
+++ src/app/main.ts
@@ -7,31 +7,48 @@ import { GaroonSchedule } from "./garoonSchedule";
 import * as ServerInfo from "./serverInfo";
 import { VobjectConverter } from "./vobjectConverter";
 
+const itemsJson = "items.json";
+
+function readItems(): types.base.ItemVersionType[] {
+    if (fs.existsSync(itemsJson)) {
+        return JSON.parse(fs.readFileSync(itemsJson, "utf-8")) as types.base.ItemVersionType[];
+    }
+    return [];
+}
+
+function writeItems(items: types.base.ItemVersionType[]) {
+    fs.writeFileSync(itemsJson, JSON.stringify(items), "utf-8");
+}
+
 async function main() {
     const serverInfo = JSON.parse(fs.readFileSync("server.json", "utf-8")) as ServerInfo.IServerInfo;
+    const items = readItems();
 
-    let garoon = new GaroonSchedule({ url: serverInfo.garoon.url });
+    const garoon = new GaroonSchedule({ url: serverInfo.garoon.url });
     garoon.authenticate(serverInfo.garoon.user, serverInfo.garoon.password);
-    let items = await garoon.getEventVersion();
+    const events = await garoon.getEventVersion(items);
 
-    let caldav = new CalDav(new URL(serverInfo.caldav.url), serverInfo.caldav.user, serverInfo.caldav.password);
+    const caldav = new CalDav(new URL(serverInfo.caldav.url), serverInfo.caldav.user, serverInfo.caldav.password);
 
-    for (let item of items) {
-        switch (item.attributes.operation) {
+    for (const x of events) {
+        switch (x.attributes.operation) {
             case "add":
-            {
-                const event = await garoon.getEvent(item.attributes.id);
-                const uuid = Uuid();
-                const vobject = VobjectConverter.fromGaroonEvent(uuid, event);
-                const response = await caldav.put(uuid + ".ics", vobject.toICS());
-                break;
-            }
+                {
+                    const event = await garoon.getEvent(x.attributes.id);
+                    const uuid = Uuid();
+                    const vobject = VobjectConverter.fromGaroonEvent(uuid, event);
+                    const response = await caldav.put(uuid + ".ics", vobject.toICS());
+                    items.push({ attributes: { id: x.attributes.id, version: x.attributes.version } });
+                    break;
+                }
             case "modify":
                 break;
             case "remove":
                 break;
         }
     }
+
+    writeItems(items);
 }
 
 main();
